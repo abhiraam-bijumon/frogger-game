9d735b738aad9dad3ad5f23b3d00ed174388cb75
Abhiraam Bijumon <abhiraambijumon@gmail.com>
2024-08-27 20:43:56 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,72 @@
-
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(img`
+    . . . . . . . . . . . c c c c c 
+    . . . . . . . . . c c 7 7 7 6 c 
+    . . . . . . . . c c 7 7 7 c c . 
+    . . . . . . . . c 6 7 7 c . . . 
+    . . . . . . . . c 6 6 6 c . . . 
+    . . . . . . . . c 6 6 6 c c . . 
+    . . . c c c c c c c 6 6 6 c c . 
+    . . c 6 7 7 7 7 6 c c 6 6 6 c . 
+    . c 7 7 7 7 7 7 7 7 c 6 6 6 c c 
+    c 6 7 7 7 7 7 7 7 7 6 c 6 6 6 c 
+    c 7 c 6 6 6 6 c 7 7 7 c 6 6 6 c 
+    f 7 c c 6 6 c c 7 7 7 f 6 6 6 c 
+    f 7 6 f 6 6 f 6 7 7 7 f 6 6 6 c 
+    . f 7 7 7 7 7 7 7 7 6 f 6 6 c . 
+    . c 1 c f f 1 c 7 6 f 6 6 c c . 
+    . c c c c c c c c c c c c . . . 
+    `, SpriteKind.Player))
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(img`
+    . . . . . . . . . . . c c c c c 
+    . . . . . . . . . c c 3 3 3 3 c 
+    . . . . . . . . c c 3 3 3 c c . 
+    . . . . . . . . c 3 3 3 c . . . 
+    . . . . . . . . c 3 3 3 c . . . 
+    . . . . . . . . c 3 3 3 c c . . 
+    . . . c c c c c c c 3 3 3 c c . 
+    . . c 3 7 7 7 7 3 c c 3 3 3 c . 
+    . c 7 7 7 7 7 7 7 7 c 3 3 3 c c 
+    c 3 7 7 7 7 7 7 7 7 3 c 3 3 3 c 
+    c 7 c 6 6 6 6 c 7 7 7 c 3 3 3 c 
+    f 7 c c 6 6 c c 7 7 7 f 3 3 3 c 
+    f 7 6 f 6 6 f 6 7 7 7 f 3 3 3 c 
+    . f 7 7 7 7 7 7 7 7 3 f 3 3 c . 
+    . c 3 c f f 3 c 7 3 f 3 3 c c . 
+    . c c c c c c c c c c c c . . . 
+    `, SpriteKind.Player))
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Three), sprites.create(img`
+    . . . . . . . . . . . c c c c c 
+    . . . . . . . . . c c 5 5 5 5 c 
+    . . . . . . . . c c 5 5 5 c c . 
+    . . . . . . . . c 5 5 5 c . . . 
+    . . . . . . . . c 5 5 5 c . . . 
+    . . . . . . . . c 5 5 5 c c . . 
+    . . . c c c c c c c 5 5 5 c c . 
+    . . c 5 7 7 7 7 5 c c 5 5 5 c . 
+    . c 7 7 7 7 7 7 7 7 c 5 5 5 c c 
+    c 5 7 7 7 7 7 7 7 7 5 c 5 5 5 c 
+    c 7 c 6 6 6 6 c 7 7 7 c 5 5 5 c 
+    f 7 c c 6 6 c c 7 7 7 f 5 5 5 c 
+    f 7 6 f 6 6 f 6 7 7 7 f 5 5 5 c 
+    . f 7 7 7 7 7 7 7 7 5 f 5 5 c . 
+    . c 5 c f f 5 c 7 5 f 5 5 c c . 
+    . c c c c c c c c c c c c . . . 
+    `, SpriteKind.Player))
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Four), sprites.create(img`
+    . . . . . . . . . . . c c c c c 
+    . . . . . . . . . c c c c c c c 
+    . . . . . . . . c c c c c c c . 
+    . . . . . . . . c c c c c . . . 
+    . . . . . . . . c c c c c . . . 
+    . . . . . . . . c c c c c c . . 
+    . . . c c c c c c c c c c c c . 
+    . . c c 7 7 7 7 c c c c c c c . 
+    . c 7 7 7 7 7 7 7 7 c c c c c c 
+    c c 7 7 7 7 7 7 7 7 c c c c c c 
+    c 7 c 6 6 6 6 c 7 7 7 c c c c c 
+    f 7 c c 6 6 c c 7 7 7 c c c c c 
+    f 7 6 f 6 6 f 6 7 7 7 f c c c c 
+    . f 7 7 7 7 7 7 7 7 c f c c c . 
+    . c c c f f c c 7 c c c c c c . 
+    . c c c c c c c c c c c c . . . 
+    `, SpriteKind.Player))
