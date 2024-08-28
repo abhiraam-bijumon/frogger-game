3352f42c903d76048ad3620abf9a18d5b6d677a6
Abhiraam Bijumon <abhiraambijumon@gmail.com>
2024-08-27 21:01:46 -0500

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,58 +1,17 @@
-mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(img`
-    . . . . . . . . . . . c c c c c 
-    . . . . . . . . . c c 7 7 7 6 c 
-    . . . . . . . . c c 7 7 7 c c . 
-    . . . . . . . . c 6 7 7 c . . . 
-    . . . . . . . . c 6 6 6 c . . . 
-    . . . . . . . . c 6 6 6 c c . . 
-    . . . c c c c c c c 6 6 6 c c . 
-    . . c 6 7 7 7 7 6 c c 6 6 6 c . 
-    . c 7 7 7 7 7 7 7 7 c 6 6 6 c c 
-    c 6 7 7 7 7 7 7 7 7 6 c 6 6 6 c 
-    c 7 c 6 6 6 6 c 7 7 7 c 6 6 6 c 
-    f 7 c c 6 6 c c 7 7 7 f 6 6 6 c 
-    f 7 6 f 6 6 f 6 7 7 7 f 6 6 6 c 
-    . f 7 7 7 7 7 7 7 7 6 f 6 6 c . 
-    . c 1 c f f 1 c 7 6 f 6 6 c c . 
-    . c c c c c c c c c c c c . . . 
-    `, SpriteKind.Player))
-mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(img`
-    . . . . . . . . . . . c c c c c 
-    . . . . . . . . . c c 3 3 3 3 c 
-    . . . . . . . . c c 3 3 3 c c . 
-    . . . . . . . . c 3 3 3 c . . . 
-    . . . . . . . . c 3 3 3 c . . . 
-    . . . . . . . . c 3 3 3 c c . . 
-    . . . c c c c c c c 3 3 3 c c . 
-    . . c 3 7 7 7 7 3 c c 3 3 3 c . 
-    . c 7 7 7 7 7 7 7 7 c 3 3 3 c c 
-    c 3 7 7 7 7 7 7 7 7 3 c 3 3 3 c 
-    c 7 c 6 6 6 6 c 7 7 7 c 3 3 3 c 
-    f 7 c c 6 6 c c 7 7 7 f 3 3 3 c 
-    f 7 6 f 6 6 f 6 7 7 7 f 3 3 3 c 
-    . f 7 7 7 7 7 7 7 7 3 f 3 3 c . 
-    . c 3 c f f 3 c 7 3 f 3 3 c c . 
-    . c c c c c c c c c c c c . . . 
-    `, SpriteKind.Player))
-mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Three), sprites.create(img`
-    . . . . . . . . . . . c c c c c 
-    . . . . . . . . . c c 5 5 5 5 c 
-    . . . . . . . . c c 5 5 5 c c . 
-    . . . . . . . . c 5 5 5 c . . . 
-    . . . . . . . . c 5 5 5 c . . . 
-    . . . . . . . . c 5 5 5 c c . . 
-    . . . c c c c c c c 5 5 5 c c . 
-    . . c 5 7 7 7 7 5 c c 5 5 5 c . 
-    . c 7 7 7 7 7 7 7 7 c 5 5 5 c c 
-    c 5 7 7 7 7 7 7 7 7 5 c 5 5 5 c 
-    c 7 c 6 6 6 6 c 7 7 7 c 5 5 5 c 
-    f 7 c c 6 6 c c 7 7 7 f 5 5 5 c 
-    f 7 6 f 6 6 f 6 7 7 7 f 5 5 5 c 
-    . f 7 7 7 7 7 7 7 7 5 f 5 5 c . 
-    . c 5 c f f 5 c 7 5 f 5 5 c c . 
-    . c c c c c c c c c c c c . . . 
-    `, SpriteKind.Player))
-mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Four), sprites.create(img`
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.y += -16
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.x += 16
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.x += -16
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.y += 16
+})
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
     . . . . . . . . . . . c c c c c 
     . . . . . . . . . c c c c c c c 
     . . . . . . . . c c c c c c c . 
@@ -69,4 +28,6 @@ mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Four), sprites.create(img`
     . f 7 7 7 7 7 7 7 7 c f c c c . 
     . c c c f f c c 7 c c c c c c . 
     . c c c c c c c c c c c c . . . 
-    `, SpriteKind.Player))
+    `, SpriteKind.Player)
+tiles.setCurrentTilemap(tilemap`level2`)
+tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 0))
---
+++ tilemap.g.ts
@@ -0,0 +1,70 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
+            case "level2":
+            case "level2":return tiles.createTilemap(hex`0a0008000300000001020000000003040000040400000000030404040400000000000304040404000000000003040400040000000000030004040000000000000300000000000000000003000000000000000000`, img`
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+`, [myTiles.transparency16,sprites.builtin.forestTiles0,myTiles.tile1,myTiles.tile5,myTiles.tile2], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "myTile1":
+            case "tile3":return tile3;
+            case "myTile2":
+            case "tile4":return tile4;
+            case "myTile3":
+            case "tile5":return tile5;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
